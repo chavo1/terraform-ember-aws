11c79d66ec5ad3d1b07cee65d16603446cfcbd41
chavo1 <chavdar.rakov@gmail.com>
2019-08-09 16:07:17 +0300

    travis

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "ember" {
   source = "./server"
 
-  region                = "${var.region}"
-  instance_type         = "${var.instance_type}"
-  key_name              = "${var.key_name}"
-  subnet                = "${var.subnet}"
-  ami                   = "${var.ami}"
+  region        = "${var.region}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
+  subnet        = "${var.subnet}"
+  ami           = "${var.ami}"
 }
